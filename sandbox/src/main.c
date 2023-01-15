bdbdae6061c15e95d3fd2f41b24420c9004ee0d8
Just-Xezon <113899383+Just-Xezon@users.noreply.github.com>
2023-01-15 11:25:10 +0100

    Add files via upload

---
+++ sandbox/src/main.c
@@ -0,0 +1,28 @@
+#include <core/logger.h>
+#include <core/asserts.h>
+
+#include <platform/platform.h>
+
+int main(void)
+{
+    HFATAL("A test message: %f", 3.14f);
+    HERROR("A test message: %f", 3.14f);
+    HWARN("A test message: %f", 3.14f);
+    HINFO("A test message: %f", 3.14f);
+    HDEBUG("A test message: %f", 3.14f);
+    HTRACE("A test message: %f", 3.14f);
+
+    platform_state state;
+
+    if (platform_startup(&state, "Hydrogen Engine Sandbox", 100, 100, 1280, 720))
+    {
+        while (TRUE)
+        {
+            platform_pump_messages(&state);
+        }
+    }
+
+    platform_shutdown(&state);
+
+    return 0;
+}
